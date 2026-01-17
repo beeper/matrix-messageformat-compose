62670f947e7a86c833b909b990f35be5352b333b
SpiritCroc <dev@spiritcroc.de>
2026-01-17 09:13:58 +0100

    JVM: Allow dollars in web urls
    As seen in matrix.to links

--- messageformat/src/jvmMain/java/com/beeper/android/messageformat/Linkify.jvm.kt
+++ messageformat/src/jvmMain/java/com/beeper/android/messageformat/Linkify.jvm.kt
@@ -1,6 +1,6 @@
 package com.beeper.android.messageformat
 
 actual val DEFAULT_WEB_URL_PATTERN = Regex(
-    """\b((?:https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])""",
+    """\b((?:https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;$]*[-a-zA-Z0-9+&@#/%=~_|$])""",
     RegexOption.IGNORE_CASE
 ).toPattern()
