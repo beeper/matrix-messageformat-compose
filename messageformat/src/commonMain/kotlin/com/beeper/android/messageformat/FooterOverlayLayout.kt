d8744813cad74ae6aa662e1034c95d4a01f43701
SpiritCroc <dev@spiritcroc.de>
2026-01-19 10:43:46 +0100

    Import FooterOverlayLayout from SchildiChat

---
+++ messageformat/src/commonMain/kotlin/com/beeper/android/messageformat/FooterOverlayLayout.kt
@@ -0,0 +1,73 @@
+package com.beeper.android.messageformat
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.unit.Dp
+import kotlin.math.max
+import kotlin.math.roundToInt
+
+/**
+ * A custom layout that inputs a [TextLayoutResult] for its main content,
+ * and overlays a footer at the bottom end, using existing free space if possible, or extending
+ * the bounds of the composable if necessary.
+ */
+@Composable
+fun TimestampOverlayLayout(
+    textLayoutResult: TextLayoutResult,
+    horizontalPadding: Dp,
+    verticalPadding: Dp,
+    content: @Composable () -> Unit,
+    overlay: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val density = LocalDensity.current
+    val horizontalPaddingPx = density.run { horizontalPadding.roundToPx() }
+    val verticalPaddingPx = density.run { verticalPadding.roundToPx() }
+    // TODO RTL?
+    Layout(
+        modifier = modifier,
+        content = {
+            content()
+            overlay()
+        }
+    ) { measurables, constraints ->
+        val textMeasurable = measurables[0]
+        val overlayMeasurable = measurables[1]
+
+        // Measure main text
+        val textPlaceable = textMeasurable.measure(constraints)
+        val overlayPlaceable = overlayMeasurable.measure(constraints)
+
+        val lastLineWidth = textLayoutResult.getLineRight(textLayoutResult.lineCount-1).roundToInt()
+        val maxAvailableWidth = constraints.maxWidth
+
+        val lastLineWidthWithHorizontalOverlay = lastLineWidth + overlayPlaceable.width + horizontalPaddingPx
+
+        if (maxAvailableWidth >= lastLineWidthWithHorizontalOverlay) {
+            // Fits into the last line
+            val width = max(textPlaceable.width, lastLineWidthWithHorizontalOverlay)
+            val height = max(textPlaceable.height, overlayPlaceable.height)
+            layout(width, height) {
+                textPlaceable.place(0, 0)
+                overlayPlaceable.place(
+                    x = width - overlayPlaceable.width,
+                    y = textPlaceable.height - overlayPlaceable.height
+                )
+            }
+        } else {
+            // Needs a new line
+            val width = max(textPlaceable.width, overlayPlaceable.width)
+            val height = textPlaceable.height + verticalPaddingPx + overlayPlaceable.height
+            layout(width, height) {
+                textPlaceable.place(0, 0)
+                overlayPlaceable.place(
+                    x = width - overlayPlaceable.width,
+                    y = textPlaceable.height + verticalPaddingPx
+                )
+            }
+        }
+    }
+}
