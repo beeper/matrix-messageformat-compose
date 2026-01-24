34be77864b1fa88bbae6a6fec1f1f0586b19f5bd
SpiritCroc <dev@spiritcroc.de>
2026-01-24 21:20:20 +0100

    Allow matrix.to room links via aliases (#11)
    * Allow matrix.to room links via aliases
    
    * Fix parsing the '#' in matrix.to links

--- messageformat/src/commonMain/kotlin/com/beeper/android/messageformat/MatrixPatterns.kt
+++ messageformat/src/commonMain/kotlin/com/beeper/android/messageformat/MatrixPatterns.kt
@@ -9,36 +9,41 @@ object MatrixPatterns {
     // This does not enforce strict rules as per spec but is a bit more permissive just in case.
     val USER_ID_REGEX = Regex("""@.*:.+""")
     val ROOM_ID_REGEX = Regex("""!.+""")
+    val ROOM_ALIAS_REGEX = Regex("""#.*:.+""")
     val MESSAGE_ID_REGEX = Regex("""\$.+""")
+
+    private fun String.isRoomIdOrAlias() =
+        ROOM_ID_REGEX.matches(this) || ROOM_ALIAS_REGEX.matches(this)
 
     fun parseMatrixToUrl(url: String): MatrixToLink? {
         if (!url.startsWith(MATRIX_TO_LINK_PREFIX)) {
             return null
         }
         val parsed = try {
-            // ktor ignores things starting with '#' so strip the prefix and treat it as server-relative url
-            Url("/" + url.removePrefix(MATRIX_TO_LINK_PREFIX))
+            // Need to escape the '#' found in plaintext in many matrix.to urls
+            // sent within matrix clients, to not confuse ktor
+            Url(url.replace("#", "%23"))
         } catch (_: Exception) {
             return null
         }
-        if (parsed.pathSegments.isEmpty()) {
+        if (parsed.pathSegments.size <= 2) {
             return null
         }
-        // First segment is just blank
-        val segments = parsed.pathSegments.subList(1, parsed.pathSegments.size)
+        // First segment is just blank, second is just the '#'
+        val segments = parsed.pathSegments.subList(2, parsed.pathSegments.size)
         when (segments.size) {
             1 -> {
                 val segment = segments.first()
                 if (USER_ID_REGEX.matches(segment)) {
                     return MatrixToLink.UserMention(segment, url)
                 }
-                if (ROOM_ID_REGEX.matches(segment)) {
+                if (segment.isRoomIdOrAlias()) {
                     val via = parsed.parameters.getAll("via")
                     return MatrixToLink.RoomLink(segment, via, url)
                 }
             }
             2 -> {
-                if (ROOM_ID_REGEX.matches(segments[0]) && MESSAGE_ID_REGEX.matches(segments[1])) {
+                if (segments[0].isRoomIdOrAlias() && MESSAGE_ID_REGEX.matches(segments[1])) {
                     val via = parsed.parameters.getAll("via")
                     return MatrixToLink.MessageLink(segments[0], segments[1], via, url)
                 }
