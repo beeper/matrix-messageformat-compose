2dbca3b4ee9cb5af8a507547bb42d15c42316ce8
SpiritCroc <dev@spiritcroc.de>
2026-02-04 19:39:59 +0100

    Android: Don't auto-linkify mid-word

--- messageformat/src/androidMain/kotlin/com/beeper/android/messageformat/Linkify.android.kt
+++ messageformat/src/androidMain/kotlin/com/beeper/android/messageformat/Linkify.android.kt
@@ -3,4 +3,8 @@ package com.beeper.android.messageformat
 import android.util.Patterns
 import java.util.regex.Pattern
 
-actual val DEFAULT_WEB_URL_PATTERN: Pattern = Patterns.WEB_URL
+actual val DEFAULT_WEB_URL_PATTERN: Pattern = Pattern.compile(
+    // Prevent the web url regex to trigger mid-word, by checking if we're at the string beginning or after some whitespace
+    "(?:(?<=\\s)|^)${Patterns.WEB_URL.pattern()}",
+    Patterns.WEB_URL.flags(),
+)
