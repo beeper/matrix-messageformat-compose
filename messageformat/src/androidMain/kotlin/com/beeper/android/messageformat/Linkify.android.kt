f876891d618966082d0c1ecf9293c7ee840cb29c
SpiritCroc <dev@spiritcroc.de>
2026-05-12 16:52:57 +0200

    Revert "Android: Don't auto-linkify mid-word (#17)"
    We want to linkify within brackets. Better to keep the system default
    as library default, and let clients bring their own if necessary.
    
    This reverts commit b66c2fd2af16125e32da4116ed0bf5553a0ed211.

--- messageformat/src/androidMain/kotlin/com/beeper/android/messageformat/Linkify.android.kt
+++ messageformat/src/androidMain/kotlin/com/beeper/android/messageformat/Linkify.android.kt
@@ -3,8 +3,4 @@ package com.beeper.android.messageformat
 import android.util.Patterns
 import java.util.regex.Pattern
 
-actual val DEFAULT_WEB_URL_PATTERN: Pattern = Pattern.compile(
-    // Prevent the web url regex to trigger mid-word, by checking if we're at the string beginning or after some whitespace
-    "(?:(?<=\\s)|^)${Patterns.WEB_URL.pattern()}",
-    Patterns.WEB_URL.flags(),
-)
+actual val DEFAULT_WEB_URL_PATTERN: Pattern = Patterns.WEB_URL
